b43fdc42d5b5514ae12ff560ef865865d00c2715
Kristjan Reinhold <kristjan.reinhold@snowhound.eu>
2022-01-30 11:23:41 +0200

    Add fb

---
+++ firebase-demo/public/app.js
@@ -0,0 +1,80 @@
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+  if (user) {
+    // signed in
+    whenSignedIn.hidden = false;
+    whenSignedOut.hidden = true;
+    userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+  } else {
+    // not signed in
+    whenSignedIn.hidden = true;
+    whenSignedOut.hidden = false;
+    userDetails.innerHTML = '';
+  }
+});
+
+
+const createThing = document.getElementById('createThing');
+const thingsList = document.getElementById('thingsList');
+
+const db = firebase.firestore();
+let thingsRef;
+let unsubscribe;
+
+auth.onAuthStateChanged(user => {
+  if (user) {
+
+    // Database Reference
+    thingsRef = db.collection('things')
+    createThing.addEventListener('click', () => {
+      const { serverTimestamp } = firebase.firestore.FieldValue;
+
+      thingsRef.add({
+        uid: user.uid,
+        name: 'xx90',
+        createdAt: serverTimestamp()
+      });
+    });
+
+  }
+});
+/*
+auth.onAuthStateChanged(user => {
+
+  if (user) {
+
+    // Database Reference
+    thingsRef = db.collection('things')
+
+    // ..... omitted .....
+
+    // Query
+    unsubscribe = thingsRef.where('uid', '==', user.uid)
+      .onSnapshot(querySnapshot => {
+
+        // Map results to an array of li elements
+
+        const items = querySnapshot.docs.map(doc => {
+
+          return `<li>${doc.data().name}</li>`
+
+        });
+
+        thingsList.innerHTML = items.join('');
+
+      });
+
+
+
+  } else {
+    // Unsubscribe when the user signs out
+    unsubscribe && unsubscribe();
+  }
+});*/
